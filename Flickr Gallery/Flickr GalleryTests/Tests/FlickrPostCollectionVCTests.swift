218ad16623b818332279c03bc913e93e1bfc8658
Sebastian <mrsebaxxl@gmail.com>
2017-08-13 17:12:14 +0200

    Add test for call to Flickr API with given URL

--- Flickr Gallery/Flickr GalleryTests/Tests/FlickrPostCollectionVCTests.swift
+++ Flickr Gallery/Flickr GalleryTests/Tests/FlickrPostCollectionVCTests.swift
@@ -57,4 +57,47 @@ class FlickrPostCollectionVCTests: XCTestCase {
         XCTAssertNotNil(collectionView.delegate,"The collectionView has no delegate")
         XCTAssertTrue(viewController === collectionView.delegate, "The viewController isn't collectionView's delegate")
     }
+    
+    // Check if call to Flickr API succedes for given URL
+    func testAsynchronousURLConnectionToFlickrAPI() {
+        let url = URL(string: "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1")!
+        let urlExpectation = expectation(description: "Should get JSON data from URL")
+        
+        // Try to get data from given URL
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            // Check the received data
+            XCTAssertNotNil(data, "data should not be nil")
+            
+            // Check if an error occurred
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                // Check the response URL
+                XCTAssertEqual(responseURL.absoluteString, url.absoluteString, "HTTP response URL should be equal to original URL")
+                
+                // Check the response code
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code should be 200")
+                
+                // Check if the content type is JSON
+                XCTAssertEqual(mimeType, "application/json", "HTTP response content type should be application/json")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            urlExpectation.fulfill()
+        }
+        
+        task.resume()
+        
+        // Wait for expectation and cancel task if timeout occurres
+        waitForExpectations(timeout: task.originalRequest!.timeoutInterval) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            task.cancel()
+        }
+    }
 }
